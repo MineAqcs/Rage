0793d4b5b1eeafe79dcc3c23f03fb9c98eb98ea9
狂想ThePotato <670067575@qq.com>
2025-05-19 20:49:35 +0800

    port to 1182

--- src/main/java/me/kall/rage/mixin/LivingEntityMixin.java
+++ src/main/java/me/kall/rage/mixin/LivingEntityMixin.java
@@ -3,15 +3,15 @@ package me.kall.rage.mixin;
 import com.llamalad7.mixinextras.sugar.Local;
 import me.kall.rage.Rage;
 import me.kall.rage.api.RageHolder;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.particles.ParticleTypes;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.SoundEvents;
-import net.minecraft.world.World;
-import net.minecraft.world.server.ServerWorld;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,13 +21,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity implements RageHolder {
-    public LivingEntityMixin(EntityType<?> pType, World pLevel) {
-        super(pType, pLevel);
-    }
 
     @Unique private volatile int rage$currentRage;
     @Unique private int rage$decreaseInterval;
-    @Unique private static final ResourceLocation PLAYER_ID = new ResourceLocation("minecraft:player");
+    @Unique private static final ResourceLocation PLAYER_ID = ResourceLocation.parse("minecraft:player");
+
+    public LivingEntityMixin(EntityType<?> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
 
     @ModifyVariable(method = "hurt", at = @At("HEAD"), argsOnly = true, ordinal = 0)
     private float rage$onHurt(float pAmount, @Local(argsOnly = true) DamageSource pSource) {
@@ -67,7 +68,7 @@ public abstract class LivingEntityMixin extends Entity implements RageHolder {
 
     @Inject(method = "tick", at = @At("TAIL"))
     private void rage$onTick(CallbackInfo ci) {
-        if (this.level instanceof ServerWorld) {
+        if (this.level instanceof ServerLevel serverWorld) {
             this.rage$decreaseInterval += 1;
             if (this.rage$decreaseInterval == Rage.DECREASE_INTERVAL_TICKS.get()) {
                 this.rage$decreaseInterval = 0;
@@ -76,7 +77,6 @@ public abstract class LivingEntityMixin extends Entity implements RageHolder {
             }
 
             if (Rage.SHOW_PARTICLE_ON_FULL_RAGE.get() && this.rage$isFullRage()) {
-                ServerWorld serverWorld = (ServerWorld) this.level;
                 serverWorld.sendParticles(ParticleTypes.CRIT, this.getX(), this.getY(), this.getZ(), 8, 0.2, 0.2, 0.2, 0.0);
             }
         }
--- src/main/java/me/kall/rage/mixin/PlayerEntityMixin.java
+++ src/main/java/me/kall/rage/mixin/PlayerEntityMixin.java
@@ -2,27 +2,27 @@ package me.kall.rage.mixin;
 
 import me.kall.rage.Rage;
 import me.kall.rage.api.RageHolder;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.particles.ParticleTypes;
-import net.minecraft.world.World;
-import net.minecraft.world.server.ServerWorld;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PlayerEntity.class)
+@Mixin(Player.class)
 public abstract class PlayerEntityMixin extends LivingEntity implements RageHolder {
-    protected PlayerEntityMixin(EntityType<? extends LivingEntity> p_i48577_1_, World p_i48577_2_) {
-        super(p_i48577_1_, p_i48577_2_);
+
+    protected PlayerEntityMixin(EntityType<? extends LivingEntity> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
     }
 
     @Inject(method = "tick", at = @At("TAIL"))
     private void rage$onTick(CallbackInfo ci) {
-        if (Rage.SHOW_PARTICLE_ON_FULL_RAGE.get() && this.rage$isFullRage() && this.level instanceof ServerWorld) {
-            ServerWorld serverWorld = (ServerWorld) this.level;
+        if (Rage.SHOW_PARTICLE_ON_FULL_RAGE.get() && this.rage$isFullRage() && this.level instanceof ServerLevel serverWorld) {
             serverWorld.sendParticles(ParticleTypes.CRIT, this.getX(), this.getY(), this.getZ(), 8, 0.2, 0.2, 0.2, 0.0);
         }
     }
